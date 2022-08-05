22ee581a12ede556bab0da241d6bfd418fa205da
KadsonLima <kadson-kaio@hotmail.com>
2022-08-04 23:28:55 -0300

    feat: get/urls/id

--- src/routes/urlsRouter.js
+++ src/routes/urlsRouter.js
@@ -1,4 +1,5 @@
 import { Router } from "express";
+import { getUrlById } from "../controllers/getUrlById.js";
 import { getUrl } from "../controllers/getUrlController.js";
 import { shortenUrl } from "../controllers/shortenController.js";
 import { validarToken } from "../middlewares/validateToken.js";
@@ -10,5 +11,6 @@ const urlRouter = Router();
 
 urlRouter.post('/urls/shorten',validarToken, shortenUrl);
 urlRouter.get('/urls/open/:shortUrl', getUrl);
+urlRouter.get('/urls/:id', getUrlById);
 
 export default urlRouter;
